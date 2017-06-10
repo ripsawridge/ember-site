fc17c96407e0136377f80279882aff3977be5a02
Mike Stanton <ripsawridge@gmail.com>
2017-06-10 04:46:40 +0200

    date format change.

---
+++ site/app/helpers/date-yyyy.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+
+function formatDate(date) {
+  var year = date.getFullYear();
+
+  return year;
+}
+
+export function dateYyyy([value, ...rest]) {
+    // Make sure we have a date.
+  if (!value.getMonth) {
+    value = new Date(value);
+  }
+
+  return formatDate(value);
+}
+
+export default Ember.Helper.helper(dateYyyy);
