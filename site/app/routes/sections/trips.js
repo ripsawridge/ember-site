6c0a4c8ef7048b97e6633f97fa3b594c76b55021
Mike Stanton <ripsawridge@gmail.com>
2017-12-05 20:06:48 +0100

    Better maps

--- site/app/routes/sections/trips.js
+++ site/app/routes/sections/trips.js
@@ -1,9 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    model: function () {
-      return Ember.$.getJSON("/articles/trips/index.json").then(function (results) {
+  model: function () {
+    return Ember.RSVP.hash({
+      trips: Ember.$.getJSON("/articles/trips/index.json").then(function (results) {
         return results;
-      });
-   }
+      }),
+      locations: Ember.$.getJSON("/articles/trips/locations.json").then(function (results) {
+        return results;
+      })
+    });
+  },
+
+  setupController(controller, model) {
+    this._super(...arguments);
+    Ember.set(controller, 'trips', model.trips);
+    Ember.set(controller, 'locations', model.locations);
+  }
 });
